8c1c9d959369724a80c60a4017ae774352d9721a
dellsystem <ilostwaldo@gmail.com>
2013-11-01 13:19:55 -0400

    Add multilingual support to CMS app

--- cms/models.py
+++ cms/models.py
@@ -1,12 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from multilingual_model.models import MultilingualModel, MultilingualTranslation
 
 
-class Page(models.Model):
-    slug = models.SlugField(unique=True)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    content = models.TextField(blank=True, null=True)
+class Page(MultilingualModel):
     custom_template = models.BooleanField(default=False)
     graphic = models.ImageField(upload_to='graphics')
 
@@ -15,3 +12,14 @@ class Page(models.Model):
 
     def get_absolute_url(self):
         return reverse('cms', args=[self.slug])
+
+
+class PageTranslation(MultilingualTranslation):
+    parent = models.ForeignKey(Page, related_name='translations')
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('parent', 'language_code')
