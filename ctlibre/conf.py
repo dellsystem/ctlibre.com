ff5385a0639e3cbe120ca4ddc1b82362a482d154
dellsystem <ilostwaldo@gmail.com>
2013-11-02 02:27:02 -0400

    Move sensitive settings to conf.py (closes #20)

---
+++ ctlibre/conf.py
@@ -0,0 +1,18 @@
+DEBUG = True
+
+DB_ENGINE = 'django.db.backends.sqlite3'
+DB_NAME = 'db.sqlite'
+DB_USER = ''
+DB_PASSWORD = ''
+DB_HOST = ''
+DB_PORT = ''
+
+SECRET_KEY = 'pkn*m*enw&020m+2s)!*re8#v_#3cg)1u5gayb7aif91+r1ov&'
+COOKIE_DOMAIN = '.ctlibre.com'
+
+ALLOWED_HOSTS = [
+    '.ctlibre.com',
+    '.ctlibre.com.',
+]
+
+INSTALLED_APPS = ()
