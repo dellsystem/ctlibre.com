37abbbaebc3002f0ebd090740c4047af9e490014
dellsystem <ilostwaldo@gmail.com>
2013-11-02 07:32:14 +0000

    Update STATIC_ROOT setting

--- ctlibre/settings.py
+++ ctlibre/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(DIRNAME, '..', 'lol')
+STATIC_ROOT = os.path.join(DIRNAME, '..', '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
